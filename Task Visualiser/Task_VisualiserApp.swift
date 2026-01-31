cd31eace7d9d97468f43f43e365093fa73aaa5d2
zak <zak.hurst@sky.com>
2026-01-31 22:12:31 +0000

    Initial Commit

---
+++ Task Visualiser/Task_VisualiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_VisualiserApp.swift
+//  Task Visualiser
+//
+//  Created by Zak Hurst on 31/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Task_VisualiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
